78bef644adacf04a7a455e3755c9718a2f6ca845
arm137039 <la227039@student.helha.be>
2024-07-02 21:49:41 +0200

    ajout du client-server

---
+++ src/main/java/server/PictionaryServer.java
@@ -0,0 +1,64 @@
+package server;
+
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class PictionaryServer {
+    private static final int PORT = 3001;
+    private ServerSocket serverSocket;
+    private List<ClientHandler> clients;
+    private List<String> words;
+
+    public PictionaryServer() {
+        clients = new ArrayList<>();
+        loadWords();
+    }
+
+    public static void main(String[] args) {
+        PictionaryServer server = new PictionaryServer();
+        server.start();
+    }
+
+    public void start() {
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Server started on port " + PORT);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(clientSocket, this);
+                clients.add(clientHandler);
+                new Thread(clientHandler).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void removeClient(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+    }
+
+    public synchronized List<ClientHandler> getClients() {
+        return clients;
+    }
+
+    private void loadWords() {
+        words = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/mots.txt")))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                words.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized String getRandomWord() {
+        Random rand = new Random();
+        return words.get(rand.nextInt(words.size()));
+    }
+}
