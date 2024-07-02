e6054b9a64f8c9fe2a9e351954f3c4c56d811f25
arm137039 <la227039@student.helha.be>
2024-07-02 15:20:56 +0200

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.ue2202_2324_labo_q3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.ue2202_2324_labo_q3 to javafx.fxml;
+    exports org.example.ue2202_2324_labo_q3;
+}
